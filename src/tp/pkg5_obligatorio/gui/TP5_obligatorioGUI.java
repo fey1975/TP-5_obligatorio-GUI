cda64c45a9f8f1957293ddb4df998da630c3cd8a
fey1975 <fabian.yapura@gmail.com>
2023-08-26 18:16:19 -0300

    Initial commit

---
+++ src/tp/pkg5_obligatorio/gui/TP5_obligatorioGUI.java
@@ -0,0 +1,27 @@
+/* Ejercicio 3: (Trabajo Práctico nro 5)
+
+  Crea una aplicación GUI en Java que gestione una lista de productos. La interfaz debe contener los siguientes elementos:
+
+  Un ComboBox (JComboBox) que permita al usuario seleccionar una categoría de producto de una lista predefinida (por ejemplo, electrónica, ropa, alimentos, etc.).
+  Un botón "Agregar producto" (JButton) que permita al usuario agregar un nuevo producto a la lista.
+  Una tabla (JTable) que muestre los productos agregados, con columnas para el nombre del producto, la categoría y el precio.
+  Cuando el usuario seleccione una categoría en el ComboBox y haga clic en el botón "Agregar producto", el programa debe permitir al usuario ingresar el nombre y
+  el precio del producto en campos de texto separados. Luego, debe agregar el producto a la tabla con la categoría seleccionada.
+
+  Además, la tabla debe ser actualizada automáticamente cada vez que se agrega un nuevo producto.
+
+  Subir proyecto a GitHub y enviar link del repositorio.
+
+ */
+package tp.pkg5_obligatorio.gui;
+
+/**
+ * @Fabian Yapura author
+ */
+public class TP5_obligatorioGUI {
+
+       public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
